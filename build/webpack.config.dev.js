55f113c7476bddec98cb2a27ab791a21cda6dc66
DonaldPeat <don@donaldpeat.com>
2018-06-18 21:02:16 -0700

    fix: change app.js to index.js (webpack default) & remove entry point from config

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -5,9 +5,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: [
-    './src/app.js'
-  ],
   devServer: {
   hot: true,
   watchOptions: {
