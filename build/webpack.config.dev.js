0488642edb5659d8941f18ebb81dfebedc1c0a0a
DonaldPeat <don@donaldpeat.com>
2018-06-18 19:35:05 -0700

    init commit: hello world w/build script

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,21 @@
+'use strict'
+//loader for single-file components
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    './src/app.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+}
