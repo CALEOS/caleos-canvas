c5e1530043aeddd54a90ad1fcb075846157302b6
DonaldPeat <don@donaldpeat.com>
2018-06-18 21:19:52 -0700

    chore: add loader for static assets

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -2,6 +2,12 @@
 const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
 
 module.exports = {
   mode: 'development',
@@ -35,6 +41,11 @@ module.exports = {
       {
         test: /\.js$/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.(js|vue)$/,
+        use: 'eslint-loader',
+        enforce: 'pre'
       }
     ]
   },
@@ -45,6 +56,11 @@ module.exports = {
       filename: 'index.html',
       template: 'index.html',
       inject: true
-    })
+    }),
+    new CopyWebpackPlugin([{
+      from: resolve('static/img'),
+      to: resolve('dist/static/img'),
+      toType: 'dir'
+    }])
   ]
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import '../assets/app.styl'
 
-
-//pull in app component render it with id 'app'
+/* eslint-disable-next-line no-new */
 new Vue({
   el: '#app',
   render: h => h(App)
