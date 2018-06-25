43e820e6a0844d37da88f12f3bbeb51558780b07
DonaldPeat <don@donaldpeat.com>
2018-06-24 18:59:56 -0700

    chore: implment vuex store for state management

--- src/components/ToolboxContainer.vue
+++ src/components/ToolboxContainer.vue
@@ -29,7 +29,10 @@ export default {
   },
   methods: {
     triggerEvent: function (e) {
+      // this.$root.store.setMessageAction('state changed from child component')
       alert('toolbox event triggered')
+      this.$store.commit('increment')
+      console.log(this.$store.state.count)
     }
   }
 }
