700d61b2b20718d0204e9c11d2bdab0b461aeeec
DonaldPeat <don@donaldpeat.com>
2018-06-24 18:59:56 -0700

    chore: implment vuex store for state management

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import '../assets/app.styl'
 import Place from './js/Place'
 import 'vue-awesome/icons/flag'
 
 Vue.prototype.$place = new Place()
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
 
 /* eslint-disable-next-line no-new */
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 })
