f49acadcdf257e72f2e1c3f15979210e5c3e034e
Jesse Schulman <jesse@caleos.io>
2019-12-14 10:37:06 -0800

    More UI cleanup

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,15 @@
   <div id="header">
     <Leaderboard class="header-item header-left" />
     <PaintHistory class="header-item header-right" />
+    <div class="header-logo-container">
+      <img
+        class="header-logo"
+        src="../../static/img/telos-logo-white.png"
+      >
+    </div>
   </div>
 </template>
 <script>
-
 import Leaderboard from './Leaderboard'
 import PaintHistory from './PaintHistory'
 // <ChatWindow class="header-item header-center" />
@@ -13,8 +18,8 @@ import PaintHistory from './PaintHistory'
 export default {
   components: {
     Leaderboard,
-    PaintHistory/*,
-    ChatWindow*/
+    PaintHistory /*,
+    ChatWindow */
   },
   props: {
     name: {
@@ -29,6 +34,12 @@ export default {
   background-color #4f2ef5
   white-space nowrap
   height 45px
+.header-logo-container
+  text-align center
+.header-logo
+  padding-top 4px
+  text-align center
+  height 37px
 .header-item
   display inline-block
 .header-left
